83e111318a9d92962977a2584cf79268eaaefc3d
Nicole Barkley <barkleylikecharles@icloud.com>
2022-06-09 00:02:13 -0400

    validation

--- models/Comment.js
+++ models/Comment.js
@@ -3,35 +3,41 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
     {
+      // set custom id to avoid confusion with parent comment _id
       replyId: {
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId()
       },
       replyBody: {
-          type: String
+        type: String,
+        required: true,
+        trim: true
       },
       writtenBy: {
-        type: String
+        type: String,
+        required: true,
       },
       createdAt: {
         type: Date,
         default: Date.now,
         get: createdAtVal => dateFormat(createdAtVal)
+      }
+    },
+    {
+      toJSON: {
+        getters: true
+      }
     }
-},
-{
-  toJSON: {
-    getters: true
-  }
-}
-);
+  );
 
 const CommentSchema = new Schema({
   writtenBy: {
-    type: String
+    type: String,
+    required: true
   },
   commentBody: {
-    type: String
+    type: String,
+    required: true
   },
   createdAt: {
     type: Date,
